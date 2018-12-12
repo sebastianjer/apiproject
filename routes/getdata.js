fa1c0452922bcb0a2984ba8fb26b66634aa0c841
sebastianjer <sebastianjeremiah5@gmail.com>
2018-12-12 21:27:05 +0700

    d3

--- routes/getdata.js
+++ routes/getdata.js
@@ -1,11 +1,51 @@
 var http = require('http');
 var d3 = require('d3-node'); //data driven documents (buat visualisasi)
-//var url = ;
+var url = 'https://data.go.id/';
 
 exports.data = function(req,res){
+  http.get(url, (res) => {
+    let data = '';
 
+    res.on('data', chunk => {
+      data += chunk;
+    })
+
+    res.on('end', () => {
+      //parse ke JSON?
+      console.log(data);
+
+    })
+  })
+
+  return data;
 }
 
 exports.visualize = function (data){
+  var width = ;
+  var height = ;
 
+  var svg = d3.select('svg')
+    .attr("width", width)
+    .attr("height", height)
+    .attr("class", "bar-chart");
+
+  var data = ;
+
+  var barPadding = 5;
+  var barWidth = (width / dataset.length);
+  var barChart = svg.selectAll("rect")
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("y", function(d) {
+        return height - d
+    })
+    .attr("height", function(d) {
+        return d;
+    })
+    .attr("width", barWidth - barPadding)
+    .attr("transform", function (d, i) {
+         var translate = [barWidth * i, 0];
+         return "translate("+ translate +")";
+    });
 }
